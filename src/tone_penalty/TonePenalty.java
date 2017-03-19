79e67a3acb3ee5feb19299b678b755db9285039f
Lei Zhao <Lei Zhao>
2017-03-19 20:53:33 +0000

    First commit

---
+++ src/tone_penalty/TonePenalty.java
@@ -0,0 +1,12 @@
+package tone_penalty;
+
+public class TonePenalty {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hellow");
+		int c = UsedForTest.doIGetIt();
+		System.out.println(c);
+	}
+
+}
---
+++ src/tone_penalty/UsedForTest.java
@@ -0,0 +1,9 @@
+package tone_penalty;
+
+public class UsedForTest {
+	public static int doIGetIt(){
+		int a = 5;
+		return a;
+	}
+	
+}
